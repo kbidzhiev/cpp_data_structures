6c10bb0682313ee67aacd052ba95922f6edbfcc1
Kemal Bidzhiev <kem.bidzhiev@gmail.com>
2026-08-07 12:49:19 +0200

    Hello world

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Data structures and algorithms in C++
+
+
+
+## How to build and run tests
+cmake -S . -B build
+cmake --build build
+ctest --test-dir build --output-on-failure
--- hello_world/tests/hw_tests.cpp
+++ hello_world/tests/hw_tests.cpp
@@ -1,9 +1,9 @@
-#include "hello_world.hpp"
+#include "hw.hpp"
 #include <gtest/gtest.h>
 
 
 TEST(Hello_world, returns) {
-  auto hw = hello_world_namespace::hello();
-  EXPECT_EQ(hw, "Hello, World");
+  auto hw = HW::hello();
+  EXPECT_EQ(hw, "Hello, world");
 }
 
